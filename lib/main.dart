a5ffc514f9e61fb6c15231e954b23099bea80b03
LapertJoseph <lapertjoseph@gmail.com>
2023-02-22 09:24:16 +0100

    catche user input with TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,9 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,16 +61,21 @@ class MyHomePage extends StatelessWidget {
                 children: [
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
                   ),
                   TextButton(
                     child: Text(
                       'Add Transaction',
                       style: TextStyle(color: Colors.purple),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountController.text);
+                    },
                   ),
                 ],
               ),
